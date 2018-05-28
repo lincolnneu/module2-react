8f19e10165c43ca90fda44f54df3cf4187390ee9
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 16:33:29 -0700

    ModuleListItem: add two icons, pend and trash from font awesom. Align the to the right.

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -4,6 +4,10 @@ export default class ModuleListItem extends React.Component{
     render(){ return( // return jsx declaration
         <li className="list-group-item">
             Module 1
+            <span className="float-right">
+                <i className="fa fa-trash"></i>
+                <i className="fa fa-pencil"></i>
+            </span>
         </li>
     );
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // entry point of an app
-import React from 'react'
-import ReactDOM from 'react-dom' // only need this in high level index
-import HelloWorld from './hello'
-import CourseManager from './containers/CourseManager'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import ReactDOM from 'react-dom'; // only need this in high level index
+import HelloWorld from './hello';
+import CourseManager from './containers/CourseManager';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <CourseManager/>,
