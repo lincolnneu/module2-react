05f58bfe0c0203ac78d0b1969d27bf66b30ad968
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 11:27:25 -0700

    added hello world index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// entry point of an app
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.getElementById('root') // default id is root
+);
