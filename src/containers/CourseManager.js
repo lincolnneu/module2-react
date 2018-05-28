84462c8cba1f2d5c34ad455481d5ea6da7fc6652
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 15:14:14 -0700

    CourseCard: added CourseCard component from bootstrap.

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,11 +1,20 @@
 // top level application
 import React, {Component} from 'react'
+import CourseCard from './CourseCard'
+
 
 export default class CourseManager extends Component{
     render(){
         return( // a bootstrap class
+            // className="card-deck: This part could be assigned as another container.js
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <div className="card-deck">
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                </div>
             </div>
         )
     }
