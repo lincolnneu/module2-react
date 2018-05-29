90e62f55677a4f1ee761a858bdee42489fc5536e
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 21:32:09 -0700

    Rendering dynamic content: render multiple elements in ReactDOM using div.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import Hello from './components/Hello'
 import Stateless from './components/Stateless'
+import ModuleListItem from "./components/ModuleListItem";
 ReactDOM.render(
-    <Stateless message={"this is a stateless component"}/>,
+    <div>
+        <ModuleListItem title="React Module"/>
+        <ModuleListItem title="Redux"/>
+        <ModuleListItem title="React Native"/>
+        <Stateless message={"this is a stateless component"}/>
+    </div>,
     document.getElementById('root') // default id is root
-);
+); // render function can only have single element. More elements can be rendered inside div.
