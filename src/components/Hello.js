8d2b3709cc8ecd53ed3f9c2a6c1f0f57ea490229
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 19:06:37 -0700

    Parameterizing react components: passing properties to constructor, parent.

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
 
 class Hello extends Component{
+    constructor(props){ // prop means property
+        super(props); // pass it to parent.
+    }
     render(){
         return(
-            <h1>Hello World!</h1>
+            <h1>{this.props.message}</h1>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import Hello from './components/Hello'
 ReactDOM.render(
-    <Hello/>,
+    <Hello message={"life is good"}/>,
     document.getElementById('root') // default id is root
 );
