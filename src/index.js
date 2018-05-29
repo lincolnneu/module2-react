dae99949807c1ed1127d7e2791440950526a9a9d
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 19:02:15 -0700

    parameterizing: hello world skeleton.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import HelloWorld from './hello';
 import CourseManager from './containers/CourseManager';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-
+import Hello from './components/Hello'
 ReactDOM.render(
-    <CourseManager/>,
+    <Hello/>,
     document.getElementById('root') // default id is root
 );
