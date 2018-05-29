9071bdd68aed33cb288dfd0db8b71bc24b9edb2b
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-28 17:28:08 -0700

    LessonTabs: Added nav bar to LessonTabs container.

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -2,13 +2,14 @@
 import React, {Component} from 'react'
 import CourseCard from '../components/CourseCard'
 import ModuleList from './ModuleList'
-
+import LessonTabs from './LessonTabs'
 export default class CourseManager extends Component{
     render(){
         return( // a bootstrap class
             // className="card-deck: This part could be assigned as another container.js
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <LessonTabs/>
                 <ModuleList/>
                 <div className="card-deck">
                     <CourseCard/>
