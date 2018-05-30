2c1b83758a883e260368780ed10303fcd442fb62
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-30 16:14:53 -0700

    CourseList: activate input field and button. If we type to the input field, we will get notified. If we click on add button, we will get notified in the console.

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -6,6 +6,8 @@ class CourseList extends React.Component{
     constructor(){
         super();
         this.courseService = CourseService.instance;
+        this.titleChanged = this.titleChanged.bind(this); // bind these methods to this component in constructor.
+        this.createCourse = this.createCourse.bind(this);
     }
 
     componentDidMount(){ // data is ready to render. Before rendering, what's your last word?
@@ -33,6 +35,13 @@ class CourseList extends React.Component{
         )
     }
 
+    titleChanged(event){ // event is a standard signature for this event handlers when infrastructure passes the event originated when the event occur. We need a reference back to the input field.
+        console.log("titleChanged");
+    }
+    createCourse(){
+        console.log("createCourse button pressed");
+    }
+
     render(){
         return(
             <div>
@@ -40,8 +49,10 @@ class CourseList extends React.Component{
                 <table className="table">
                     <thead>
                         <tr><th>Title</th></tr>
-                        <th><input className="form-control" id="titleFld" placeholder="cs101"/></th>
-                        <th><button className="btn btn-primary">Add</button></th>
+                        <th><input onChange={this.titleChanged}
+                            className="form-control" id="titleFld" placeholder="cs101"/></th>
+                        <th><button onClick={this.createCourse}
+                            className="btn btn-primary">Add</button></th>
                     </thead>
                     <tbody>
                         {this.renderCourseRows()}
