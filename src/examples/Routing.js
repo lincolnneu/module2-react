02cbfa693e87ff2d16e6d2da3f9c65310f01e08b
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-29 19:51:26 -0700

    navigation: created an example routing.

---
+++ src/examples/Routing.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Routing = () => {
+    return (<h1>Hello World!</h1>);
+};
+
+export default Routing;
--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ import Stateless from './components/Stateless'
 import ModuleListItem from "./components/ModuleListItem";
 import ModuleList2 from "./containers/ModuleList2"
 // import ModuleList from "./containers/ModuleList"
+import Routing from "./examples/Routing"
 
 // className={"container-fluid"} add some paddings to both side.
 ReactDOM.render(
     <div className={"container-fluid"}>
-        <ModuleList2/>
-        <Stateless message={"this is a stateless component"}/>
+        <Routing/>
+        {/*<ModuleList2/>*/}
+        {/*<Stateless message={"this is a stateless component"}/>*/}
     </div>,
     document.getElementById('root') // default id is root
 ); // render function can only have single element. More elements can be rendered inside div.
