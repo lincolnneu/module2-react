04f898b286b58affa528afc72226b72ea2f3d9ea
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-30 11:11:50 -0700

    list: Added CourseRow into CourseList.

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import CourseRow from "../components/CourseRow";
 class CourseList extends React.Component{
     constructor(){ // dummy constructor doesn't do anything
         super();
     }
     render(){
         return(
-            <h2>Course List</h2>
+            <div>
+                <h2>Course List</h2>
+                <table className="table">
+                    <thead><tr><th>Title</th></tr></thead>
+                    <tbody>
+                        <CourseRow/>
+                        <CourseRow/>
+                        <CourseRow/>
+                        <CourseRow/>
+                        <CourseRow/>
+                        <CourseRow/>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
