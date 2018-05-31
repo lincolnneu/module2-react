e3fcdbe73c697ee926590ef969ed7e24499d10df
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-30 17:13:46 -0700

    navigation: CourseList will only be displayed in the courses route. Each course in the list is a hyperlink to corresponding course editor page.

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -4,26 +4,39 @@ import CourseCard from '../components/CourseCard'
 import ModuleList from './ModuleList'
 import LessonTabs from './LessonTabs'
 import CourseEditor from './CourseEditor'
-import CourseList from "./CourseList";
+import CourseList from "./CourseList"
+import {BrowserRouter as Router, Route} from 'react-router-dom' // call BrowserRouter locally as Router.
 
 export default class CourseManager extends Component{
     render(){
-        return( // a bootstrap class
-            // className="card-deck: This part could be assigned as another container.js
-            <div className="container-fluid">
-                <h1>Course Manager</h1>
-                <CourseList/>
-                <div className="card-deck">
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
+        return(
+            <Router>
+                {/* a bootstrap class */}
+                {/* className="card-deck: This part could be assigned as another container.js */}
+                <div className="container-fluid">
+                    <h1>Course Manager</h1>
+                    <Route path="/courses" component={CourseList}>
+                    {/*display the courselist only when we navigate to /courses*/}
+                    </Route>
+                    <Route path="/course/:courseId/edit"
+                           component={CourseEditor}>
+                    </Route>
+                    {/*<Route path="/examples">*/}
+                        {/*<div>*/}
+                            {/*<div className="card-deck">*/}
+                                {/*<CourseCard/>*/}
+                                {/*<CourseCard/>*/}
+                                {/*<CourseCard/>*/}
+                                {/*<CourseCard/>*/}
+                            {/*</div>*/}
+                            {/*<CourseEditor/>*/}
+                            {/*<br/>*/}
+                            {/*<LessonTabs/>*/}
+                            {/*<ModuleList/>*/}
+                        {/*</div>*/}
+                    {/*</Route>*/}
                 </div>
-                <CourseEditor/>
-                <br/>
-                <LessonTabs/>
-                <ModuleList/>
-            </div>
+            </Router>
         )
     }
 }
