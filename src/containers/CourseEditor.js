9218f1cb30ee51afd0b86c4bbae017abb8ef8ec3
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-30 17:29:30 -0700

    getting json data from a REST API: After clicking on course link in the course list, we will be navigated to its editor page, with its course id displayed.

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -5,14 +5,32 @@ import LessonTabs from './LessonTabs'
 
 export default class CourseEditor
     extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {courseId: ''};
+        this.selectCourse = this.selectCourse.bind(this);
+    }
+
+    componentDidMount(){// set course id. When it loads, we parse the parameter. When the component did mount, we call select and extract id.
+        this.selectCourse(this.props.match.params.courseId);
+    }
+
+    selectCourse(courseId){
+        this.setState({courseId: courseId});
+    }
+
+
     render(){
         return (
-            <div className="row">
-                <div className="col-4">
-                    <ModuleList/>
-                </div>
-                <div className="col-8">
-                    <LessonTabs/>
+            <div>
+                <h2>Editing course: {this.state.courseId}</h2>
+                <div className="row">
+                    <div className="col-4">
+                        <ModuleList/>
+                    </div>
+                    <div className="col-8">
+                        <LessonTabs/>
+                    </div>
                 </div>
             </div>
         )
