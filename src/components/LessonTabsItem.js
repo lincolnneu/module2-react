723ca32dedf5ea60fd9f771a70a315354a7102de
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-02 22:53:00 -0700

    Lesson: the building block of a lesson item in LessonsTab.

---
+++ src/components/LessonTabsItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default class ModuleListItem extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        let me = this;
+        return( // return jsx declaration
+            <div>
+                <li className="nav-item">
+                    <span className="nav-link active" href="#">
+                        <Link to = {`/course/${this.props.courseId}/module/${this.props.moduleId}/lesson/${this.props.lesson.id}/edit`}>
+                            {this.props.title}
+                        </Link>
+                        <i onClick={function(){me.props.deleteLesson(me.props.lesson.id);}} className="fa fa-times"></i>
+                    </span>
+                </li>
+            </div>
+        );
+    }
+}
