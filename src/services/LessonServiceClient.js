d4059fe6820b4138d078d79827f9f372da97d6e9
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-02 22:53:56 -0700

    Lesson: Implemented a singleton that encapsulates all lesson related data access to/from the server about Lesson.

---
+++ src/services/LessonServiceClient.js
@@ -0,0 +1,41 @@
+const LESSON_API_URL = 'http://localhost:8080/api/course/CID/module/MID/lesson';
+
+let _singleton = Symbol();
+export default class LessonServiceClient { // this service is a singleton
+    constructor(singletonToken){
+        if(_singleton !== singletonToken){
+            throw new Error('Singleton!!!');
+        }
+    }
+
+    findAllLessonsForModule(courseId,moduleId){
+        return fetch(LESSON_API_URL.replace('CID', courseId).replace('MID',moduleId))
+                .then(function(response){
+                return response.json();
+            });
+    }
+
+
+    createLesson(courseId, moduleId, lesson){
+        return fetch(LESSON_API_URL.replace('CID', courseId).replace('MID',moduleId),{ // CID is a place holder
+            body: JSON.stringify(lesson),
+            headers: {'Content-Type' : 'application/json'},
+            method: 'POST'
+        }).then(function(response){
+            return response.json();
+        })
+    }
+
+    deleteLesson(lessonId){
+        return fetch(LESSON_API_URL.replace('course/CID/module/MID/', '') + '/' + lessonId, {
+            method: 'DELETE'
+        });
+    }
+
+    static get instance(){
+        if(!this[_singleton]) {
+            this[_singleton] = new LessonServiceClient(_singleton);
+        }
+        return this[_singleton];
+    }
+}
