d608cb494a4828a7afa0010af1d341a6eeb004a2
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-04 00:42:54 -0700

    moduleList: enable highlighting module item when it is selected

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -2,11 +2,38 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default class ModuleListItem extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            attribute:'',
+            moduleId:'',
+            curModuleId:''
+        }
+        this.setAttribute = this.setAttribute.bind(this);
+    }
+
+    setAttribute(){
+        console.log("16 setting attribute!");
+        this.setState({attribute:this.props.attribute});
+        console.log(this.state.attribute);
+    }
+
+    componentDidMount(){
+        this.setAttribute();
+    }
+
+
     render(){
         let me = this;
+        console.log("24 in moduleItem");
+        console.log(me.state.curModuleId)
         return( // return jsx declaration
-        <li className="list-group-item">
-            <Link to = {`/course/${this.props.courseId}/module/${this.props.module.id}`}>
+        <li className={`list-group-item ${this.state.attribute}`}>
+            <Link to = {`/course/${this.props.courseId}/module/${this.props.module.id}`}
+            onClick={function(){
+                console.log("26 this module is clicked!")
+                this.setAttribute();
+            }}>
                 {this.props.title}
             </Link>
 
--- src/containers/ModuleList.js
+++ src/containers/ModuleList.js
@@ -11,19 +11,28 @@ class ModuleList extends React.Component {
         this.state = {
             courseId: '',
             module: { title: '' },
-            modules: []
+            modules: [],
+            curModuleId: ''
         };
 
         this.titleChanged = this.titleChanged.bind(this);
         this.createModule = this.createModule.bind(this);
         this.setCourseId = this.setCourseId.bind(this);
         this.deleteModule = this.deleteModule.bind(this);
+        this.setCurModuleId = this.setCurModuleId.bind(this);
         this.moduleService = ModuleServiceClient.instance; // once we have the instance we can call it in the createModule.
 
     }
 
     setModules(modules){
         this.setState({modules: modules});
+    }
+
+    setCurModuleId(){
+        let curModule = window.location.href.split('/')[6];
+        console.log(curModule);
+        this.setState({curModuleId: curModule});
+
     }
 
     findAllModulesForCourse(courseId){
@@ -36,6 +45,7 @@ class ModuleList extends React.Component {
     }
 
     componentDidMount(){
+        this.setCurModuleId();
         this.setCourseId(this.props.courseId);
     }
 
@@ -73,7 +83,16 @@ class ModuleList extends React.Component {
     renderListOfModules() {
         let me = this;
         let modules = this.state.modules.map(function (module) {
-            return <ModuleListItem key={module.id} title={module.title} courseId={me.props.courseId} module={module} deleteModule={me.deleteModule}/>
+            let at = '';
+            console.log(module);
+            console.log(module.id);
+            console.log(me.state.curModuleId);
+            if(me.state.curModuleId == module.id){
+                at = 'list-group-item-danger';
+                console.log("91 oh yeah, that's curModule. Now at is");
+                console.log(at);
+            }
+            return <ModuleListItem key={module.id} attribute={at} title={module.title} courseId={me.props.courseId} module={module} deleteModule={me.deleteModule}/>
         });
         return modules;
     }
@@ -92,7 +111,7 @@ class ModuleList extends React.Component {
                                 <i className="fa fa-plus"></i>
                             </button>
 
-                            <ul className="list-group nav nav-tabs">
+                            <ul className="list-group">
                                 {this.renderListOfModules()}
                             </ul>
                         </div>
