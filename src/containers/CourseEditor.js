9685e8ddae0f63c738ea9b5f13e4adcd08dc6ef5
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-17 17:41:15 -0700

    course && lesson: fixed navbar bug.

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -45,7 +45,7 @@ export default class CourseEditor
     render(){
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top box-shadow">
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top box-shadow">
                     <div className="container-fluid d-flex justify-content-between">
                         <div className="navbar-header">
                             <a className="navbar-brand align-items-center d-flex" href="/courses">{this.state.course.title}</a>
@@ -59,8 +59,8 @@ export default class CourseEditor
                                 aria-controls="bd-docs-nav" aria-expanded="false" aria-label="Toggle docs navigation">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30"
                                  focusable="false"><title>Menu</title>
-                                <path stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                      stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path>
+                                <path stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                      strokeMiterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path>
                             </svg>
                         </button>
                     </div>
--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -96,7 +96,7 @@ class CourseList extends React.Component{
         let me = this;
         return(
             <div>
-                <nav className="navbar navbar-expand-lg bg-dark fixed-top">
+                <nav className="navbar navbar-expand-md bg-dark fixed-top">
                     <div className="container-fluid">
                         <div className="navbar-header">
                             <a className="navbar-brand" href="/courses" style={{color : "#FFF"}}>Course Manager</a>
@@ -118,8 +118,8 @@ class CourseList extends React.Component{
                                 aria-controls="bd-docs-nav" aria-expanded="false" aria-label="Toggle docs navigation">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30"
                                  focusable="false"><title>Menu</title>
-                                <path stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                      stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path>
+                                <path stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                      strokeMiterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path>
                             </svg>
                         </button>
 
